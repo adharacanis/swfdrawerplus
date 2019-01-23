6133cb582c65c13b26eeee03d78e26650fb72b63
konstantink <konstantink@playtika.com>
2019-01-23 17:04:41 +0300

    Initial Commit

---
+++ WIthTheBoost/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WIthTheBoost
+//
+//  Created by Konstantin Klementiev on 1/23/19.
+//  Copyright © 2019 Konstantin Klementiev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
